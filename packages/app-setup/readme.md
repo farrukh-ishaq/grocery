71f250df4fdd1cbdae8f746bdd25bb95b008daf8
farrukhishaq <farrukh.ishaq@homeoffice.gov.uk>
2025-09-01 20:49:32 +0100

    redis, user and migration sorted.

--- packages/app-setup/readme.md
+++ packages/app-setup/readme.md
@@ -6,3 +6,6 @@ docker-compose up
 
 # Force rebuild if you've changed the Docker configuration
 docker-compose up -d --build
+
+# medusa user for development
+`medusa user -e admin@example.com -p supersecretpassword -f Admin -l User -r admin`
--- packages/web/src/app/lib/medusa.ts
+++ packages/web/src/app/lib/medusa.ts
@@ -3,7 +3,7 @@ import Medusa from "@medusajs/medusa-js"
 const medusa = new Medusa({
     baseUrl: "http://localhost:9000",
     maxRetries: 3,
-    publishableApiKey: "pk_0bd3a1213f63ccf2ada2b5b6754e413a2ef95b786b5f6a5c8174554464e33409"
+    publishableApiKey: "pk_89e8f3bde7a55c29adaadfea682fe8aafc8d9b129508124722d7839602493910"
 })
 
 export default medusa
